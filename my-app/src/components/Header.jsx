4380be5238e07df98ab2f17b3c055f356793133a
gabrielvolponi <gabriel.volponi@vidibr.com>
2025-06-29 13:11:26 -0300

    solving conflits 2

--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -2,7 +2,7 @@ import Nav from './Nav'
 
 function Header(props) {
     return <div className="header">
-        <img src={props.src} />
+        <img src={props.src} alt='Logo' />
         <Nav paginaAtual={props.paginaAtual} />
     </div> 
 }
