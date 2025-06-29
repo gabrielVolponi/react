20037899f6e8d90fa4d4b8d687de1a7f26356130
gabrielvolponi <gabriel.volponi@vidibr.com>
2025-06-29 13:16:49 -0300

    hero finalizado

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Header src='https://images.unsplash.com/photo-1597871040916-4b4c20ba08dd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' paginaAtual="precos" />
+     <Hero src='https://images.unsplash.com/photo-1659168915827-1ccf35a141d0?q=80&w=1142&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' h2='Uma experiência Incrível' h4='Conheça Agora'/>
     </>
     
   )
