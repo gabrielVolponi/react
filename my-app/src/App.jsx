e6666d35385f53b05d0b3d4f31144875868cb664
gabrielvolponi <gabriel.volponi@vidibr.com>
2025-06-29 14:31:28 -0300

    cardSection

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-     <Header src='https://images.unsplash.com/photo-1597871040916-4b4c20ba08dd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' paginaAtual="sobre" />
+     <Header src='https://images.unsplash.com/photo-1597871040916-4b4c20ba08dd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' paginaAtual="sobre"/>
      <Hero src='https://images.unsplash.com/photo-1659168915827-1ccf35a141d0?q=80&w=1142&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' h2='Uma experiência Incrível' h4='Conheça Agora'/>
      <SectionPublico h2='Pra quem esse passeio foi feito'/>
     </>
--- my-app/src/components/CardSection.jsx
+++ my-app/src/components/CardSection.jsx
@@ -1,5 +1,5 @@
 function CardSection(props) {
-    <div className="cardSection">
+    return <div className="cardSection">
         <img src={props.src} alt={props.titulo} />
         <div className="cardSectionText">
             <h4>{props.titulo}</h4>
