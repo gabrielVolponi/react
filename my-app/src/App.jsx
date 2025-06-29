97e293d72a1e95bb17fcc7ab5653b1b318a15f41
gabrielvolponi <gabriel.volponi@vidibr.com>
2025-06-29 15:18:47 -0300

    footer finalizado + short out icon

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import SectionPublico from './components/SectionPublico'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
      <Header src='https://images.unsplash.com/photo-1597871040916-4b4c20ba08dd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' paginaAtual="sobre"/>
      <Hero src='https://images.unsplash.com/photo-1659168915827-1ccf35a141d0?q=80&w=1142&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' h2='Uma experiência Incrível' h4='Conheça Agora'/>
      <SectionPublico h2='Pra quem esse passeio foi feito'/>
+      <Footer/>
     </>
     
   )
