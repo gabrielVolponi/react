d6e1743222fdffb3cd949e3fa87e745ae1f38d0e
Vasseli <nicolaszielasko@gmail.com>
2025-06-29 15:24:15 -0300

    Continuação arquitetura

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
-import './App.css'
 import Header from './components/Header'
-import Preco from './pages/Preco';
 import Sobre from './pages/Sobre';
-import Contato from './pages/Contato';
+import { Routes, Route } from 'react-router-dom';
 
 
 function App() {
@@ -13,9 +11,7 @@ function App() {
     <>
     <Header src='https://images.unsplash.com/photo-1597871040916-4b4c20ba08dd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' paginaAtual="sobre" />
         <Routes>
-        <Route path="../pages/Sobre" element={<Sobre />} />
-        <Route path="../pages/Preco"  element={<Preco />} />
-        <Route path="../pages/Contato"  element={<Contato />} />
+        <Route path="/" element={<Sobre />} />
       </Routes>
 
     </>
--- my-app/src/pages/Sobre.jsx
+++ my-app/src/pages/Sobre.jsx
@@ -1,7 +1,7 @@
-import Hero from './components/Hero'
-import SectionPublico from './components/SectionPublico'
+import Hero from '../components/Hero'
+import SectionPublico from '../components/SectionPublico'
 
-export default function Home () {
+export default function Sobre () {
     return (
     <div>
         <Hero src='https://images.unsplash.com/photo-1659168915827-1ccf35a141d0?q=80&w=1142&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' h2='Uma experiência Incrível' h4='Conheça Agora'/>
