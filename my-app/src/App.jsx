c35a3f9a6852410f0c58081e1b81d1fdc933e24d
Vasseli <nicolaszielasko@gmail.com>
2025-06-29 14:32:03 -0300

    Alteração na arquitetura

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
-import Hero from './components/Hero'
+import Home from './pages/Home';
+import Sobre from './pages/Sobre';
+import Contato from './pages/Contato';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Header src='https://images.unsplash.com/photo-1597871040916-4b4c20ba08dd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' paginaAtual="sobre" />
-     <Hero src='https://images.unsplash.com/photo-1659168915827-1ccf35a141d0?q=80&w=1142&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' h2='Uma experiência Incrível' h4='Conheça Agora'/>
+    <Header src='https://images.unsplash.com/photo-1597871040916-4b4c20ba08dd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' paginaAtual="sobre" />
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
     </>
     
   )
---
+++ my-app/src/pages/Sobre.jsx
@@ -0,0 +1,9 @@
+import Hero from './components/Hero'
+
+export default function Home () {
+    return (
+    <div>
+        <Hero src='https://images.unsplash.com/photo-1659168915827-1ccf35a141d0?q=80&w=1142&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' h2='Uma experiência Incrível' h4='Conheça Agora'/>
+    </div>)
+
+}
