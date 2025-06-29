cdd68cc505a4ebcda9501c04971b87eb0fe7d767
Vasseli <nicolaszielasko@gmail.com>
2025-06-29 12:59:13 -0300

    nav interativa

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,19 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/header'
-import Nav from './components/Nav'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <Header src='https://images.unsplash.com/photo-1597871040916-4b4c20ba08dd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+     <Header src='https://images.unsplash.com/photo-1597871040916-4b4c20ba08dd?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' paginaAtual="precos" />
     </>
     
   )
 }
 
 export default App
+
--- my-app/src/components/Header.jsx
+++ my-app/src/components/Header.jsx
@@ -1,11 +1,13 @@
-import Nav from './Nav'
+
+import Nav from './Nav';
 
 function Header(props) {
-    return <div className="header">
-        <img src={props.src} />
-        <Nav className=''/>
-    </div> 
+  return (
+    <div className="header">
+      <img src={props.src} alt="Logo" />
+      <Nav paginaAtual={props.paginaAtual} />
+    </div>
+  );
 }
 
-
-export default Header
+export default Header;
